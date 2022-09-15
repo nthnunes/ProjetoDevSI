399052ec2e638a3b319513815a7f3f00a8f94a5b
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-09-15 14:59:17 -0300

    add routes: login, register, password

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, Response, jsonify, request
+from bson.objectid import ObjectId
+from database import dbConnection
+from users import User
+
+print("$ MongoDB Credentials:")
+db = dbConnection(input("Username: "), input("Password: "))
 
 opc = int(input("Run app:\n1 - Local\n2 - In my network\n3 - TCP Tunnel\nOption: "))
 
@@ -11,9 +17,47 @@ if opc == 3:
 def homepage():
     return jsonify({"Name": "Condominium API", "Status": "OK", "Author": "Nathan Nunes"})
 
+
+@app.route('/login')
+def login():
+    req = request.get_json()
+    coll = db.get_collection('users')
+    data = coll.find_one({'email': req['email']})
+
+    if data == None:
+        return Response(status=403)
+
+    if data['senha'] == req['senha']:
+        return jsonify({'id': str(data['_id']), 'permissao': data['permissao']})
+    else:
+        return Response(status=401)
+    
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return request.json
+    req = request.get_json()
+    user = User(req['email'], req['senha'], req['nome'], req['telefone'], False)
+
+    data = {
+        "email": user.getEmail(),
+        "senha": user.getSenha(),
+        "nome": user.getNome(),
+        "telefone": user.getTelefone(),
+        "permissao": user.getPermissao()
+    }
+    coll = db.get_collection('users')
+    coll.insert_one(data)
+    
+    return Response(status=200)
+
+
+@app.route('/password')
+def changePassword():
+    req = request.get_json()
+    coll = db.get_collection('users')
+    coll.update_one({"_id": ObjectId(req['id'])}, {'$set': {"senha": req['password']}})
+
+    return Response(status=200)
 
 
 if __name__ == '__main__':
