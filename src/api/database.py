0270e4e450fa3bbe1312ea9f7be76955b1d660af
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-09-15 14:57:20 -0300

    change to return only devsi database

--- src/api/database.py
+++ src/api/database.py
@@ -4,14 +4,4 @@ def dbConnection(username, password):
     CONNECTION_STRING = "mongodb+srv://" + username + ":" + password + "@cluster0.bbxnito.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(CONNECTION_STRING)
 
-    return client
-
-username = input("$ MongoDB Connection\nUsername: ")
-password = input("Password: ")
-
-client = dbConnection(username, password)
-
-print(client.list_database_names())
-
-db = client.get_database('')
-col = db.get_collection('')
+    return client.get_database('devsi')
