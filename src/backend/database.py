f8e93fb08c0599041de409375a8fa92b8454b9e6
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-09-23 12:08:42 -0300

    Delete database.py

--- src/backend/database.py
+++
@@ -1,7 +0,0 @@
-def dbConnection(username, password):
-    from pymongo import MongoClient
-
-    CONNECTION_STRING = "mongodb+srv://" + username + ":" + password + "@cluster0.bbxnito.mongodb.net/?retryWrites=true&w=majority"
-    client = MongoClient(CONNECTION_STRING)
-
-    return client.get_database('devsi')
