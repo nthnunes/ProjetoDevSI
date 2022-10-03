892a40451265c674b2780aac314cf3d56119697d
Nathan Nunes <nathanpedronunes@hotmail.com>
2022-10-03 14:18:34 -0300

    att

--- src/frontend/js/script.js
+++ src/frontend/js/script.js
@@ -1,4 +1,32 @@
-import api from './api.js'
+async function api(url, body, method, res){
+    url = "https://devsiproject.vercel.app" + url
+
+    const headers = new Headers({
+        "Content-Type": "application/json"
+    })
+
+    const payload = {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body)
+    }
+
+    let req = await fetch(url, payload)
+        .then(response => response)
+
+    if(req.status == 200) {
+        if(res == true){
+            let data = await req.json()
+            return {"req": req, "data": data}
+        }
+        return {"req": req}
+    }
+    
+    return new PromiseRejectionEvent("unhandledrejection", {
+        promise: req,
+        reason: "Failed request."
+    })
+}
 
 function login(){
     event.preventDefault()
